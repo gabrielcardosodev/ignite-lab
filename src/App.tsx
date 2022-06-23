caa613aefd57b49b956f5c64dedb33026a1991b0
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-06-23 11:00:52 -0300

    feat(components): add router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { ApolloProvider } from '@apollo/client'
+import { BrowserRouter } from 'react-router-dom'
 
 import { client } from './lib/apollo'
-import { Event } from './pages/Event'
+import { Router } from './Router'
 
 export function App() {
   return (
     <div>
       <ApolloProvider client={client}>
-        <Event />
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
       </ApolloProvider>
     </div>
   )
