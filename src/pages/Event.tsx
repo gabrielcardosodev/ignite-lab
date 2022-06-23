2e17aad948c80fd4335539892c2f8f36cc22b443
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-06-23 11:03:53 -0300

    feat(components): create slug param to access lesson video

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -1,13 +1,17 @@
+import { useParams } from 'react-router-dom'
+
 import { Header } from '../components/Header'
 import { Sidebar } from '../components/Sidebar'
 import { Video } from '../components/Video'
 
 export function Event() {
+  const { slug } = useParams<{ slug: string }>()
+
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <main className="flex flex-1">
-        <Video />
+        {slug ? <Video lessonSlug={slug} /> : <div className="flex-1" />}
         <Sidebar />
       </main>
     </div>
