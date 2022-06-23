dec8c2dff0d453ae02f98c3056bc3dfd2a2b851d
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-06-23 11:02:50 -0300

    feat(components): replace static data for api data

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,5 +1,8 @@
+import { gql, useQuery } from '@apollo/client'
+import { DefaultUi, Player, Youtube } from '@vime/react'
 import {
   CaretRight,
+  CircleNotch,
   DiscordLogo,
   FileArrowDown,
   Image,
@@ -8,40 +11,90 @@ import {
 
 import { Footer } from './Footer'
 
-export function Video() {
+import '@vime/core/themes/default.css'
+
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonResponse($slug: String) {
+    lesson(where: { slug: $slug }) {
+      title
+      videoId
+      description
+      teacher {
+        bio
+        avatarURL
+        name
+      }
+    }
+  }
+`
+
+interface VideoProps {
+  lessonSlug: string
+}
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string
+    videoId: string
+    description: string
+    teacher: {
+      bio: string
+      avatarURL: string
+      name: string
+    }
+  }
+}
+
+export function Video({ lessonSlug }: VideoProps) {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: lessonSlug
+    },
+    fetchPolicy: 'no-cache'
+  })
+
+  if (!data) {
+    return (
+      <div className="flex-1">
+        <div className="2xl:mt[10vh] mx-0 mt-[30vh] flex items-center justify-center">
+          <CircleNotch className="animate-spin" size={32} />
+        </div>
+      </div>
+    )
+  }
+
   return (
     <>
       <div className="flex-1">
         <div className="flex justify-center bg-black">
-          <div className="aspect-video h-full max-h-[60vh] w-full max-w-[1100px]"></div>
+          <div className="aspect-video h-full max-h-[60vh] w-full max-w-[1100px]">
+            <Player>
+              <Youtube videoId={data.lesson.videoId} />
+              <DefaultUi />
+            </Player>
+          </div>
         </div>
 
         <div className="mx-auto max-w-[1100px] p-6">
           <div className="flex items-start gap-16">
             <div className="flex-1">
               <h1 className="text-2xl font-bold text-gray-100">
-                Aula 01 - Criando o projeto e realizando o setup inicial
+                {data.lesson.title}
               </h1>
-              <p className="mt-4 text-gray-200">
-                Nessa aula vamos dar início ao projeto criando a estrutura base
-                da aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos
-                também realizar o setup do nosso projeto no GraphCMS criando as
-                entidades da aplicação e integrando a API GraphQL gerada pela
-                plataforma no nosso front-end utilizando Apollo Client.
-              </p>
+              <p className="mt-4 text-gray-200">{data.lesson.description}</p>
 
               <div className="mt-6 flex items-center gap-4">
                 <img
                   className="h-16 w-16 rounded-full border-2 border-blue-300"
-                  src="https://avatars.githubusercontent.com/u/83722145?v=4"
+                  src={data.lesson.teacher.avatarURL}
                 />
 
                 <div className="flex flex-col leading-relaxed">
                   <strong className="text-2xl text-gray-100">
-                    Diego Fernandes
+                    {data.lesson.teacher.name}
                   </strong>
                   <span className="text-sm text-gray-300">
-                    Co-fundador e CTO na Rocketseat
+                    {data.lesson.teacher.bio}
                   </span>
                 </div>
               </div>
