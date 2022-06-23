e6d741635d216a2b453a1784fb45085451d9d776
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-06-23 14:51:28 -0300

    style(components): add some styles

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,7 +1,7 @@
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/esm/locale/pt-BR'
 import { CheckCircle, Lock } from 'phosphor-react'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 interface LessonProps {
   title: string
@@ -11,6 +11,7 @@ interface LessonProps {
 }
 
 export function Lesson({ title, slug, availableAt, type }: LessonProps) {
+  const { slug: slugRef } = useParams<{ slug: string }>()
   const isAvailable = isPast(availableAt)
   const availableDateFormatted = format(
     availableAt,
@@ -27,10 +28,20 @@ export function Lesson({ title, slug, availableAt, type }: LessonProps) {
     >
       <span className="text-gray-300">{availableDateFormatted}</span>
 
-      <div className="mt-2 rounded border border-gray-600 p-4 transition-all group-hover:border-blue-500">
+      <div
+        className={`${
+          slugRef &&
+          slugRef === slug &&
+          'bg-blue-500 before:absolute before:left-[-7px] before:top-1/3 before:h-[14px] before:w-[14px] before:rotate-45 before:bg-blue-500 before:content-[""]'
+        } relative  mt-2 rounded border border-gray-600 p-4 transition-all group-hover:border-blue-500`}
+      >
         <header className="flex justify-between">
           {isAvailable ? (
-            <span className="flex items-center gap-2 text-sm font-medium text-blue-300">
+            <span
+              className={`${
+                slugRef && slugRef === slug ? 'text-white' : 'text-blue-300'
+              } flex items-center gap-2 text-sm font-medium`}
+            >
               <CheckCircle size={20} />
               Conteúdo liberado
             </span>
@@ -41,12 +52,22 @@ export function Lesson({ title, slug, availableAt, type }: LessonProps) {
             </span>
           )}
 
-          <span className="flex items-center rounded border border-blue-500 px-2 py-[0.125rem] text-xs font-bold uppercase text-white">
+          <span
+            className={`${
+              slugRef && slugRef === slug ? 'border-white' : 'border-blue-500'
+            } flex items-center rounded border px-2 py-[0.125rem] text-xs font-bold uppercase text-white`}
+          >
             {type === 'live' ? 'Ao vivo' : 'Aula prática'}
           </span>
         </header>
 
-        <p className="mt-4 font-bold text-gray-200">{title}</p>
+        <p
+          className={`mt-4 font-bold ${
+            slugRef && slugRef === slug ? 'text-white' : 'text-gray-200'
+          }`}
+        >
+          {title}
+        </p>
       </div>
     </Link>
   )
