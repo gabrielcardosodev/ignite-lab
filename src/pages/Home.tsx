09ddc2f18afb7498c791111da2daeb4e4fc1727a
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-06-24 10:40:44 -0300

    style(pages): make some improves in styles on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,11 +11,12 @@ export function Home() {
 
   return (
     <>
-      <main className="mx-auto my-0 flex h-screen w-full max-w-[1100px] flex-col justify-center">
-        <div className="mt-20 flex justify-between gap-52 px-10">
-          <div>
+      <main className="mx-auto flex min-h-screen flex-col items-center">
+        <div className="mx-auto mt-32 flex w-full max-w-[1100px] items-center justify-between">
+          <div className="w-full max-w-[640px]">
             <Logo />
-            <h1 className="mt-8 text-[40px] text-gray-100">
+
+            <h1 className="mt-8 text-[2.5rem] text-gray-100">
               Construa uma{' '}
               <span className="font-medium text-blue-300">
                 aplicação completa,
@@ -23,6 +24,7 @@ export function Home() {
               do zero, com{' '}
               <span className="font-medium text-blue-300">React JS</span>
             </h1>
+
             <p className="mt-6 text-gray-200">
               Em apenas uma semana você vai dominar na prática uma das
               tecnologias mais utilizadas e com alta demanda para acessar as
@@ -30,23 +32,29 @@ export function Home() {
             </p>
           </div>
 
-          <div className="rounded bg-gray-600 p-8">
-            <form onSubmit={handleSubmitForm}>
-              <span className="text-2xl font-bold text-gray-100">
-                Inscrever-se gratuitamente
-              </span>
+          <div className="rounded border border-gray-600 bg-gray-700 p-8">
+            <span className="mb-6 block text-2xl font-bold text-gray-100">
+              Inscrever-se gratuitamente
+            </span>
+
+            <form
+              className="flex w-full flex-col gap-2"
+              onSubmit={handleSubmitForm}
+            >
               <input
-                className="mt-6 mb-2 h-14 w-full rounded border border-transparent bg-gray-900 px-4 text-gray-100 placeholder:text-gray-300 focus:border-blue-500 focus:outline-none"
+                className="h-14 rounded border border-transparent bg-gray-900 px-5 text-gray-100 placeholder:text-gray-300 focus:border-blue-500 focus:outline-none"
                 placeholder="Seu nome completo"
                 type="text"
               />
+
               <input
                 type="email"
                 placeholder="Digite seu email"
-                className="h-14 w-full rounded border border-transparent bg-gray-900 px-4 text-gray-100 placeholder:text-gray-300 focus:border-blue-500 focus:outline-none"
+                className="h-14 rounded border border-transparent bg-gray-900 px-5 text-gray-100 placeholder:text-gray-300 focus:border-blue-500 focus:outline-none"
               />
+
               <button
-                className="mt-6 h-14 w-full rounded bg-blue-500 px-4 text-sm font-bold uppercase text-white transition-colors hover:opacity-90"
+                className="mt-4 h-14 rounded bg-blue-500 py-4 text-sm font-bold uppercase text-white transition-colors hover:opacity-90"
                 type="submit"
               >
                 Garantir minha vaga
