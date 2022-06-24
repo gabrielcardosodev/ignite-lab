c1c52ee460c89badcf79927bba2f60086c6fb249
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-06-24 12:01:32 -0300

    feat(components): add a link in header to navigate to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom'
+
 import { Logo } from './Logo'
 
 export function Header() {
   return (
     <header className="flex w-full items-center justify-center border-b border-gray-600 bg-gray-700 py-5">
-      <Logo />
+      <Link to="/">
+        <Logo />
+      </Link>
     </header>
   )
 }
