92ff927d8ee1a1da3e4d4bf57f7009956230c95f
Gabriel Cardoso <gabrielcardosodeveloper@gmail.com>
2022-06-28 15:07:40 -0300

    feat: set in local storage the user token for you don't have to login again

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { FormEvent, useEffect, useState } from 'react'
 import { toast, Toaster } from 'react-hot-toast'
 import { useNavigate } from 'react-router-dom'
 
+import codeImg from '../assets/code.png'
 import { Footer } from '../components/Footer'
 import { Logo } from '../components/Logo'
 import { useCreateSubscriberMutation } from '../graphql/generated'
@@ -15,6 +16,8 @@ export function Home() {
 
   const navigate = useNavigate()
 
+  const alreadySubscribe = localStorage.getItem('subscribed')
+
   useEffect(() => {
     if (error) {
       toast.error('Ocorreu um erro ao processar sua inscrição!')
@@ -26,6 +29,8 @@ export function Home() {
       toast.success(
         'Inscrição realizada! você será redirecionado para a página do evento em 3 segundos.'
       )
+
+      localStorage.setItem('subscribed', 'true')
 
       setTimeout(() => {
         navigate('/event/lesson/abertura')
@@ -50,7 +55,7 @@ export function Home() {
 
   return (
     <>
-      <main className="mx-auto flex min-h-screen flex-col items-center">
+      <main className="flex min-h-screen flex-col items-center bg-blur bg-cover bg-center bg-no-repeat">
         <div className="mx-auto mt-10 flex w-full max-w-[1100px] flex-col items-center justify-between gap-8 lg:mt-32 lg:flex-row lg:gap-0">
           <div className="flex w-full max-w-[312px] flex-col items-center justify-center md:max-w-[512px] lg:max-w-[640px] lg:items-start">
             <Logo />
@@ -64,7 +69,7 @@ export function Home() {
               <span className="font-medium text-blue-300">React JS</span>
             </h1>
 
-            <p className="mt-6 text-center text-gray-200 lg:text-left">
+            <p className="mt-6 text-center text-sm text-gray-200 lg:text-left lg:text-base">
               Em apenas uma semana você vai dominar na prática uma das
               tecnologias mais utilizadas e com alta demanda para acessar as
               melhores oportunidades do mercado.
@@ -72,39 +77,56 @@ export function Home() {
           </div>
 
           <div className="rounded border border-gray-600 bg-gray-700 p-8 lg:p-8">
-            <span className="mb-6 block text-2xl font-bold text-gray-100">
-              Inscrever-se gratuitamente
-            </span>
+            {!alreadySubscribe ? (
+              <>
+                <span className="mb-6 block text-2xl font-bold text-gray-100">
+                  Inscrever-se gratuitamente
+                </span>
 
-            <form
-              className="flex w-full flex-col gap-2"
-              onSubmit={handleSubmitForm}
-            >
-              <input
-                type="text"
-                placeholder="Seu nome completo"
-                className="h-14 rounded border border-transparent bg-gray-900 px-5 text-gray-100 placeholder:text-gray-300 focus:border-blue-500 focus:outline-none"
-                onChange={e => setName(e.target.value)}
-              />
+                <form
+                  className="flex w-full flex-col gap-2"
+                  onSubmit={handleSubmitForm}
+                >
+                  <input
+                    type="text"
+                    placeholder="Seu nome completo"
+                    className="h-14 rounded border border-transparent bg-gray-900 px-5 text-gray-100 placeholder:text-gray-300 focus:border-blue-500 focus:outline-none"
+                    onChange={e => setName(e.target.value)}
+                  />
 
-              <input
-                type="email"
-                placeholder="Digite seu email"
-                className="h-14 rounded border border-transparent bg-gray-900 px-5 text-gray-100 placeholder:text-gray-300 focus:border-blue-500 focus:outline-none"
-                onChange={e => setEmail(e.target.value)}
-              />
+                  <input
+                    type="email"
+                    placeholder="Digite seu email"
+                    className="h-14 rounded border border-transparent bg-gray-900 px-5 text-gray-100 placeholder:text-gray-300 focus:border-blue-500 focus:outline-none"
+                    onChange={e => setEmail(e.target.value)}
+                  />
 
-              <button
-                disabled={loading}
-                className="mt-4 h-14 rounded bg-blue-500 py-4 text-sm font-bold uppercase text-white transition-colors hover:opacity-90 disabled:opacity-50"
-                type="submit"
-              >
-                Garantir minha vaga
-              </button>
-              <Toaster position="top-right" reverseOrder={false} />
-            </form>
+                  <button
+                    disabled={loading}
+                    className="mt-4 h-14 rounded bg-blue-500 py-4 text-sm font-bold uppercase text-white transition-colors hover:opacity-90 disabled:opacity-50"
+                    type="submit"
+                  >
+                    Garantir minha vaga
+                  </button>
+                  <Toaster position="top-right" reverseOrder={false} />
+                </form>
+              </>
+            ) : (
+              <div className="flex w-full flex-col gap-2">
+                <span className="mb-6 block text-2xl font-bold text-gray-100">
+                  Você está inscrito!
+                </span>
+                <button
+                  className="mt-4 h-14 rounded bg-blue-500 py-4 text-sm font-bold uppercase text-white transition-colors hover:opacity-90 disabled:opacity-50"
+                  onClick={() => navigate('event/lesson/abertura')}
+                >
+                  Ir para o evento
+                </button>
+              </div>
+            )}
           </div>
         </div>
+        <img src={codeImg} alt="snipped de um código" />
       </main>
       <Footer />
     </>
